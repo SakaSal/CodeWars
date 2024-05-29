4f48fef4a522f90ef2aa89639e09fdac17493330
SakaSal <SakaSal@SakaMail.cum>
2024-05-29 16:18:29 -0700

    pig latin at work

--- PigLatin.py
+++ PigLatin.py
@@ -6,7 +6,12 @@ Leave punctuation marks untouched.
 pig_it('Pig latin is cool') # igPay atinlay siay oolcay
 pig_it('Hello world !')     # elloHay orldway !
 '''
+text='Pig latin is cool'
+splitText = text.split(' ')
+print(splitText)
 
 def pig_it(text):
-    # your code here
-    exit()
+    splitText = text.split(' ')
+    print(splitText[0][0])
+
+pig_it(text)
